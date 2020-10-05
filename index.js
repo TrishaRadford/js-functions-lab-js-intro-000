57ebe0200daaae4f7538c8f5c2ff57d3658e00d8
convivial-reducer-7231 <tradford10@gmail.com>
2020-10-05 15:24:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ function happyHolidaysTo (name) {
 function happyCustomHolidayTo (holiday, name) {
   return `Happy ${holiday}, ${name}!`;
 }
-function holidayCountdown (string) {
-  var days = "10"
-
+function holidayCountdown (days, holiday) {
+  return `It's ${days} days until ${holiday}!`
 }
