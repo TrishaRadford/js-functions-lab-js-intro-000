30d423176ba1061495b562b8765f2f28caf21072
convivial-reducer-7231 <tradford10@gmail.com>
2020-10-05 14:26:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ function happyHolidays (string) {
 }
 function happyHolidaysTo (string) {
   var name = "Tom"
-  console.log('Happy holidays, ${name}!');
+  console.log(`Happy holidays, ${name}!`);
 }
